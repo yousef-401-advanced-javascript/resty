59e4560dc14ae9745c2ead715ca80e9d99f1e6ba
yousef-97 <alshenyousef@gmail.com>
2020-06-29 22:51:32 +0300

    Updates

---
+++ precache-manifest.21ed29b6c3283a554c75e5d6f2b9cc45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a5741f79866e4a90bb93526d6aabbb",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "b69d057ced5e8019a943",
+    "url": "/resty/static/css/main.26024cff.chunk.css"
+  },
+  {
+    "revision": "64909891a31834ed1312",
+    "url": "/resty/static/js/2.25b57a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.25b57a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b69d057ced5e8019a943",
+    "url": "/resty/static/js/main.5534bfd4.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
